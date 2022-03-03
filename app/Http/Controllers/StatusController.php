9878f677fec10bc88520f0693091c99129105dd0
Sergi Baro <sergi.baro@revo.works>
2022-03-03 22:43:30 +0100

    Return printer status online or offline.

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Redis;
 
 class StatusController extends Controller
@@ -10,6 +11,13 @@ class StatusController extends Controller
     {
         $id = $printerIdentifier ?? request('uuid');
         $date = Redis::lpop("CloudPrinter_".$id);
-        return response(["lastSeen" => $date]);
+        if (!$date) {
+            return response([$id => "OFFLINE"]);
+        }
+
+        $date = Carbon::createFromFormat('Y-m-d H:i:s',$date);
+        $seconds = $date->diffInSeconds(Carbon::now());
+
+        return response([$id => ($seconds < 20 ? "ONLINE" : "OFFLINE")]);
     }
 }
